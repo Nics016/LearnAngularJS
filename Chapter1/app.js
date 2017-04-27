c4086931adc37d85fcc0a596bd150d1232d83a14
Nikita <nics009@yandex.ru>
2017-04-27 15:24:28 +0300

    Refactored directives into a separate file products.js

--- Chapter1/app.js
+++ Chapter1/app.js
@@ -1,45 +1,5 @@
 (function(){
-	var app = angular.module('store', [ ]);
-
-	app.directive('productGallery', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'product-gallery.html',
-			controller: function(){
-				this.current = 0;
-				this.setCurrent = function(imageNumber){
-					this.current = imageNumber || 0;
-				};
-			},
-			controllerAs: 'gallery',
-		}
-	});
-
-	app.directive('productTitle', function(){
-		return {
-			restrict: 'E', // html-element
-			templateUrl: 'product-title.html',
-		};
-	});
-
-	app.directive('productPanels', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'product-panels.html',
-			controller: function(){
-				this.tab = 1;
-
-				this.selectTab = function(setTab){
-					this.tab = setTab;
-				};
-
-				this.isSelected = function(checkTab){
-					return (this.tab === checkTab);
-				}
-			},
-			controllerAs: 'panel',
-		}
-	});
+	var app = angular.module('store', ['store-directives']);
 
 	app.controller('StoreController', function(){
 		this.products = gems;
@@ -58,7 +18,7 @@
 		{
 			name: 'Dodecahedron',
 			price: 2,
-			description: '. . .',
+			description: 'Cool gem which looks like Diamond',
 			canPurchase: true,
 			images: [
 				'https://scontent.cdninstagram.com/t51.2885-19/s150x150/14533737_186649848471908_7275876467181879296_a.jpg',
@@ -79,7 +39,7 @@
 		{
 			name: 'Pentagonal Gem',
 			price: 5.95,
-			description: '. . .',
+			description: 'That might be Korund (Rubin)',
 			canPurchase: true,
 			images: [
 				'https://sites.google.com/site/korundhlinik/_/rsrc/1472873022017/home/videogalerie/korund-cerveny.jpg?height=200&width=200',
